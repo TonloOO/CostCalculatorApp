ea0d6ade706850258bbc38b32918c266a1b094c7
lzs0603 <lzs0603>
2024-09-24 17:27:18 +0800

    Initial Commit

---
+++ CostCalculatorApp/CostCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CostCalculatorAppApp.swift
+//  CostCalculatorApp
+//
+//  Created by Zishuo Li on 2024-09-24.
+//
+
+import SwiftUI
+
+@main
+struct CostCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CostCalculatorAppTests/CostCalculatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CostCalculatorAppTests.swift
+//  CostCalculatorAppTests
+//
+//  Created by Zishuo Li on 2024-09-24.
+//
+
+import Testing
+@testable import CostCalculatorApp
+
+struct CostCalculatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
